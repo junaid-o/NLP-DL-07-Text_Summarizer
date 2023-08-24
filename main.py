863f6bf35396bc20d62e475e0372891fad1a85e5
junaid-o <junaidmohd795@gmail.com>
2023-08-24 16:37:34 +0530

    Data Validation Done!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 #from TextSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from TextSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from TextSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -20,3 +20,13 @@ except Exception as e:
         raise TextSummarizerException(e, sys) from e
 
 
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -2,9 +2,10 @@ from TextSummarizer.constants import *
 from TextSummarizer.utils.utils import read_yaml, create_directories
 from TextSummarizer.entity import (DataIngestionConfig,
                                    DataValidationConfig,
-                                   DataTransformationConfig,
-                                   ModelTrainerConfig,
-                                   ModelEvaluationConfig)
+                                   #DataTransformationConfig,
+                                   #ModelTrainerConfig,
+                                   #ModelEvaluationConfig
+                                   )
 
 
 class ConfigurationManager:
@@ -33,3 +34,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
